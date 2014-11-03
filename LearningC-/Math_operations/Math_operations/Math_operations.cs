23543eaa2c584971f3383ced3d56934447f75b1b
unknown-user <unknown-user@172.16.78.131>
2014-11-03 13:39:38 -0800

    Changing type of class to static

--- LearningC-/Math_operations/Math_operations/Math_operations.cs
+++ LearningC-/Math_operations/Math_operations/Math_operations.cs
@@ -9,32 +9,34 @@ namespace Math_operations
     {
         static void Main(string[] args)
         {
-            Math_operations ob = new Math_operations();
-            double add_result = ob.addition(2, 3);
-            double sub_result = ob.subtraction(2, 3);
-            double mult_result = ob.multiplication(2, 3);
-            double div_result = ob.division(2, 3);
-            Console.WriteLine(add_result);
-            Console.WriteLine(sub_result);
-            Console.WriteLine(mult_result);
-            Console.WriteLine(div_result);
+            double resultOfAddition = Math.addition(2, 3);
+			Console.WriteLine(resultOfAddition);
+
+			double resultOfSubstraction = Math.subtraction(2, 3);
+			Console.WriteLine(resultOfSubstraction);
+
+            double resultOfMultiplication = Math.multiplication(2, 3);
+			Console.WriteLine(resultOfMultiplication);
+
+            double resultOfDivision = Math.division(2, 3);
+			Console.WriteLine(resultOfDivision);
         }
     }
-    class Math_operations 
+    static class Math
     {
-        public double addition(double a, double b) 
+        static public double addition(double a, double b) 
         {
             return a + b;
         }
-        public double subtraction(double a, double b)
+		static public double subtraction(double a, double b)
         {
             return a - b;
         }
-        public double multiplication(double a, double b)
+		static public double multiplication(double a, double b)
         {
             return a * b;
         }
-        public double division(double a, double b)
+		static public double division(double a, double b)
         {
             return a / b;
         }
