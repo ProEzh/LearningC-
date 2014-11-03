050d253478865a92e79d0c13fd7fd8b83d4a96fa
ProEzh <valera199206@mail.ru>
2014-11-03 22:59:32 +0400

    new file:   LearningC-/Math_operations/Math_operations.sln 	new file:   LearningC-/Math_operations/Math_operations.suo 	new file:   LearningC-/Math_operations/Math_operations/Math_operations.cs 	new file:   LearningC-/Math_operations/Math_operations/Math_operations.csproj 	new file:   LearningC-/Math_operations/Math_operations/Properties/AssemblyInfo.cs 	new file:   LearningC-/Math_operations/Math_operations/bin/Debug/Math_operations.exe 	new file:   LearningC-/Math_operations/Math_operations/bin/Debug/Math_operations.pdb 	new file:   LearningC-/Math_operations/Math_operations/bin/Debug/Math_operations.vshost.exe 	new file:   LearningC-/Math_operations/Math_operations/bin/Debug/Math_operations.vshost.exe.manifest 	new file:   LearningC-/Math_operations/Math_operations/obj/x86/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   LearningC-/Math_operations/Math_operations/obj/x86/Debug/Math_operations.csproj.FileListAbsolute.txt 	new file:   LearningC-/Math_operations/Math_operations/obj/x86/Debug/Math_operations.exe 	new file:   LearningC-/Math_operations/Math_operations/obj/x86/Debug/Math_operations.pdb

---
+++ LearningC-/Math_operations/Math_operations/Math_operations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Math_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math_operations ob = new Math_operations();
+            double add_result = ob.addition(2, 3);
+            double sub_result = ob.subtraction(2, 3);
+            double mult_result = ob.multiplication(2, 3);
+            double div_result = ob.division(2, 3);
+            Console.WriteLine(add_result);
+            Console.WriteLine(sub_result);
+            Console.WriteLine(mult_result);
+            Console.WriteLine(div_result);
+        }
+    }
+    class Math_operations 
+    {
+        public double addition(double a, double b) 
+        {
+            return a + b;
+        }
+        public double subtraction(double a, double b)
+        {
+            return a - b;
+        }
+        public double multiplication(double a, double b)
+        {
+            return a * b;
+        }
+        public double division(double a, double b)
+        {
+            return a / b;
+        }
+
+    }
+}
